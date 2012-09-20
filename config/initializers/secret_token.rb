6c445564401b0a1041e50c1b0b92aed2510d5292
Aloha-dev <abhijeetk@alohatechnology.com>
2012-09-20 13:53:08 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Herokutest::Application.config.secret_token = '9452ee0a23555dac2aadd818c05d75817aa0417754c82080c853f591c3f3c23b3964118e088bcb5cd400a9f734f2fae32e341d6085555c6bf8c14f254d4b38dc'
